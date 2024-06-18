666fe77196100d69c285decd83c8d46485d569aa
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-06-18 13:52:50 +0300

    Modified Main.kt: Added variables, implemented null safety checks

--- HelloWorld/src/main/kotlin/Main.kt
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -7,4 +7,13 @@ fun main() {
     val age = 10
 
     var isMale = true
+
+    val message : String? = null
+
+    val number = null  // Böyle yaparsan tip Nothing? olur
+
+    var result: Int? = 0
+    result!!.plus(32) //Burada null olma ihtimali var ama !! ile null olmayacağını belirttik
+    result?.plus(32) //Burada null ise işlem yapmaz null değilse işlem yapar
+
 }
