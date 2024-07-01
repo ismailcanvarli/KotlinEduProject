2b5672c4fac2f92da384a031e74b1339af02bb65
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-07-01 14:14:09 +0300

    Code reformated

--- HelloWorld/src/main/kotlin/Main.kt
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -12,7 +12,7 @@ fun main() {
 
     var result: Int? = 0
     result!!.plus(32) //Burada null olma ihtimali var ama !! ile null olmayacağını belirttik
-    result?.plus(32) //Burada null ise işlem yapmaz null değilse işlem yapar
+    result.plus(32) //Burada null ise işlem yapmaz null değilse işlem yapar
 
     val yas = 30
     val name: String = "Kotlin"
