68a5e74ff431a278b0f27ec8e0ee6a1f78fb7f42
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-07-03 14:35:49 +0300

    Added examples of operator usage in Kotlin

---
+++ HelloWorld/src/main/kotlin/controlflow/Operators.kt
@@ -0,0 +1,31 @@
+package org.example.controlflow
+
+fun main() {
+    var a = 10
+    val b = 5
+
+    // Operatörler
+    println("a++ = ${a++}") // 10
+    println("++a = ${++a}") // 12
+    println("a-- = ${a--}") // 12
+    println("--a = ${--a}") // 10
+
+    // Operatör fonksiyonlarının kullanım sebepi
+    //Eğer değerlerden birisi veya her ikisi null ise normal operatörler kullanılamaz
+    println(a.plus(b)) // 15
+    println(a.minus(b)) // 5
+    println(a % b) // 0
+    println(a / b) // 2
+
+    println(a > b) // true
+    println(a < b) // false
+    println(a >= b) // true
+    println(a <= b) // false
+    println(a == b) // false
+    println(a != b) // true
+
+    var number1 = 10
+    var number2 = 20
+    var flag = true
+
+}
