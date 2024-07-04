1e73f21a3fcd8600eea1073cc056bcdd3f7966c3
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-07-04 14:19:15 +0300

    Added exercises on Kotlin while loops

---
+++ HelloWorld/src/main/kotlin/loop/Loops-While.kt
@@ -0,0 +1,28 @@
+//Created by canVarli on 7/4/2024.
+
+package org.example.loop
+
+fun main() {
+    /**
+     * While döngüsü belirli bir koşul sağlandığı sürece çalışır.
+     * While döngüsü, döngü başlamadan önce koşul kontrol edilir.
+     * For ve if birleştirilmiş hali gibi.
+     * İş hayatında çok kullanılmaz. Genelde for ve for if
+     */
+
+    var number = 0
+
+    while (number < 5) {
+        println("Number: $number") // 0, 1, 2, 3, 4
+        number++
+    }
+
+    // While döngüsü ile aynı işlemi for döngüsü ile yapalım.
+    for (value in 0..4) {
+        if (value < 5) {
+            println("Value: $value") // 0, 1, 2, 3, 4
+        } else {
+            break
+        }
+    }
+}
