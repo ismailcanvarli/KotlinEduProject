d0f68df6eed812d336a3c7f5fea6920a5be2dae6
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-07-05 14:25:09 +0300

    Completed an exercise to body Mass Index (BMI) calculator with weight status output

---
+++ HelloWorld/src/main/kotlin/testquestion/BodyMassIndexCalculator.kt
@@ -0,0 +1,46 @@
+//Created by canVarli on 7/5/2024.
+/**
+ * Vücut kitle indeksi hesaplayan bir algoritma yazınız:
+ *
+ *         - Vücut kitle indeksi kilo/(boy*boy) şeklinde bulunur
+ *         - Kullanıcıdan boy(cm) ve Kilo(kg) şeklinde istesin
+ *         - Boyu metre cinsine çevirsin
+ *         - Ekrana vücut kitle indeksini ve durumunu yazdırsın
+ *
+ *         # 0-20 =  Zayıf
+ *         # 20-25 = Ortalama
+ *         # 25-30 = Kilolu
+ *         # 30-oo = Obez
+ *
+ * Örnek çıktı: vücut kitle indeksiniz 24 Normal kilodasınız:
+ */
+
+package org.example.testquestion
+
+fun main() {
+    println("Boyunuzu santimetre (cm) cinsinden giriniz: ")
+    val heightCm = readln().toDouble()
+
+    println("Kilonuzu kilogram (kg) cinsinden giriniz: ")
+    val weight = readln().toDouble()
+
+    calculateBodyMassIndex(heightCm, weight)
+}
+
+fun calculateBodyMassIndex(heightCm: Double, weight: Double) {
+    val heightM = heightCm / 100
+    val bmi: Double = (weight / (heightM * heightM))
+
+    println("Vücut kitle indeksiniz: $bmi")
+    printWeightStatus(bmi)
+}
+
+fun printWeightStatus(bmi: Double) {
+    when {
+        bmi < 20 -> println("Zayıf")
+        bmi < 25 -> println("Ortalama")
+        bmi < 30 -> println("Kilolu")
+        else -> println("Obez")
+    }
+}
+
