db0f48460dd8e26118f1ef72f2f5512a71f8df13
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-07-12 13:55:59 +0300

    Add example demonstrating encapsulation in Kotlin

---
+++ HelloWorld/src/main/kotlin/classes/Encapsulation.kt
@@ -0,0 +1,13 @@
+//Created by canVarli on 7/12/2024.
+
+package org.example.classes
+
+class Encapsulation {
+    val name: String = "Ismail Can"
+    val surname: String = "Varlı"
+
+    fun getFullName(): String {
+        return "$name $surname"
+    }
+
+}
