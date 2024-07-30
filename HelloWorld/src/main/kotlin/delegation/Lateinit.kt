849da9f09050033cbb3283d0bff37a1832b2fa7e
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-07-30 16:43:38 +0300

    Add example demonstrating usage of 'lateinit' for late initialization in Kotlin

---
+++ HelloWorld/src/main/kotlin/delegation/Lateinit.kt
@@ -0,0 +1,39 @@
+//Created by canVarli on 7/30/2024.
+
+package org.example.delegation
+
+/**
+ * Late initialization: lateinit ile tanımlanan property'lerin değeri sadece
+ * tanımlandığı andan itibaren erişilebilir hale gelir.
+ *
+ * lateinit ile tanımlanan property'lerin değeri null olamaz.
+ * lateinit ile tanımlanan property'lerin değeri sadece var olabilir.
+ */
+
+class MyClass {
+    lateinit var lateInitProperty: String
+
+    fun initailize() {
+        lateInitProperty = "Initialized"
+    }
+
+    fun printProperty() {
+        // Eğer lateInitProperty henüz başlatılmadıysa, bir exception fırlatılır.
+        // isInitialized ile property'nin başlatılıp başlatılmadığını kontrol edebiliriz.
+        // Eğer başlatılmadıysa, exception fırlatmadan önce kontrol edebiliriz.
+        if (::lateInitProperty.isInitialized) {
+            println(lateInitProperty)
+        } else {
+            initailize()
+            printProperty()
+        }
+    }
+}
+
+fun main() {
+    val myClass = MyClass()
+    myClass.printProperty() // Çıktı: Initialized
+
+    myClass.initailize()
+    myClass.printProperty() // Çıktı: Initialized
+}
