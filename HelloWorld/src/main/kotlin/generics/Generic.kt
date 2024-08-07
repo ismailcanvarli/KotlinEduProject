52e7ca78500b207c72af435a59d54919afd39c1f
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-08-07 22:43:39 +0300

    Refactor: Update code and comments.

--- HelloWorld/src/main/kotlin/generics/Generic.kt
+++ HelloWorld/src/main/kotlin/generics/Generic.kt
@@ -7,7 +7,6 @@ package org.example.generics
  * Bunun anlamı List<String>, List<Object>'nin bir alt tipi değil.
  * Mesela int için bir number diyorduk. Ancak burada bir dönüşüm olamyacak.
  * Sub type'ları super type'lar yerine kullanamayız.
- *
  */
 
 // Bu invariant bir generic type'dır
@@ -20,7 +19,6 @@ class Source<out T> {
     fun nextT(): T {
         return value as T
     }
-
     val value: Any = "String"
 }
 
@@ -31,19 +29,13 @@ class Comparable<in T> {
     }
 }
 
-
 fun main() {
-
     val box = Box(1) // Box<Int> türünde bir nesne oluşturuldu.
     val box1 = Box("asd") // Box<String> türünde bir nesne oluşturuldu.
 
     // Kendisini yada alt sınıfı bekliyorsa co-variant kullanabiliriz.
     // buna örnek olarak List<Any> -> List<String> verilebilir.
 
-
-
     // Kendisini yada üst sınıfı bekliyorsa contra-variant kullanabiliriz.
     // buna örnek olarak List<String> -> List<Any> verilebilir.
-
-
 }
